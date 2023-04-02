ce3b007a3d823605243214521720fbc5f3a4c4b7
ultimathul3 <ultimathul3@ya.ru>
2023-04-02 15:23:22 +0300

    add migrations

---
+++ migrations/001_init.up.sql
@@ -0,0 +1,61 @@
+CREATE TABLE users (
+    "id" BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    "login" TEXT UNIQUE NOT NULL,
+    "name" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL
+);
+
+CREATE TABLE notebooks (
+    "id" BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    "description" TEXT NOT NULL,
+    "user_id" BIGINT REFERENCES users("id") ON DELETE CASCADE NOT NULL
+);
+
+CREATE TABLE notes (
+    "id" BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    "title" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL,
+    "updated_at" TIMESTAMPTZ NOT NULL,
+    "user_id" BIGINT REFERENCES users("id") ON DELETE CASCADE NOT NULL,
+    "notebook_id" BIGINT REFERENCES notebooks("id") ON DELETE CASCADE NOT NULL
+);
+
+CREATE TABLE files (
+    "id" BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    "filename" TEXT NOT NULL,
+    "note_id" BIGINT REFERENCES notes("id") ON DELETE CASCADE NOT NULL,
+    UNIQUE("filename", "note_id")
+);
+
+CREATE TABLE shared_notes (
+    "id" BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    "who_id" BIGINT REFERENCES users("id") ON DELETE CASCADE NOT NULL,
+    "is_whom_id" BIGINT REFERENCES users("id") ON DELETE CASCADE NOT NULL,
+    "note_id" BIGINT REFERENCES notes("id") ON DELETE CASCADE NOT NULL,
+    "accepted" BOOLEAN NOT NULL
+);
+
+CREATE TABLE todo_lists (
+    "id" BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    "title" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL,
+    "updated_at" TIMESTAMPTZ NOT NULL,
+    "user_id" BIGINT REFERENCES users("id") ON DELETE CASCADE NOT NULL,
+    "notebook_id" BIGINT REFERENCES notebooks("id") ON DELETE CASCADE NOT NULL
+);
+
+CREATE TABLE todo_items (
+    "id" BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    "body" TEXT NOT NULL,
+    "done" BOOLEAN NOT NULL,
+    "todo_list_id" BIGINT REFERENCES todo_lists("id") ON DELETE CASCADE NOT NULL
+);
+
+CREATE TABLE shared_todo_lists (
+    "id" BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    "who_id" BIGINT REFERENCES users("id") ON DELETE CASCADE NOT NULL,
+    "is_whom_id" BIGINT REFERENCES users("id") ON DELETE CASCADE NOT NULL,
+    "todo_list_id" BIGINT REFERENCES todo_lists("id") ON DELETE CASCADE NOT NULL,
+    "accepted" BOOLEAN NOT NULL
+);
