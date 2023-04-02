b97641847175b3497bc92b745b8f991c696a5267
ultimathul3 <ultimathul3@ya.ru>
2023-04-02 18:49:39 +0300

    add config package

---
+++ internal/config/config.go
@@ -0,0 +1,26 @@
+package config
+
+import "github.com/ilyakaznacheev/cleanenv"
+
+type Config struct {
+	HTTP struct {
+		IP   string `env:"HTTP_IP"`
+		Port uint16 `env:"HTTP_PORT"`
+	}
+
+	PostgreSQL struct {
+		Username string `env:"PSQL_USERNAME"`
+		Password string `env:"PSQL_PASSWORD"`
+		Host     string `env:"PSQL_HOST"`
+		Port     uint16 `env:"PSQL_PORT"`
+		DB       string `env:"PSQL_DB"`
+	}
+}
+
+func ReadEnvFile() (*Config, error) {
+	var cfg Config
+
+	err := cleanenv.ReadConfig(".env", &cfg)
+
+	return &cfg, err
+}
