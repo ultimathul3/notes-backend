0bfec4ee709d3d6149e7d936d4041006f0c41746
ultimathul3 <ultimathul3@ya.ru>
2023-04-15 23:00:45 +0300

    add test for get user ID usecase

--- internal/user/usecase_test.go
+++ internal/user/usecase_test.go
@@ -17,36 +17,60 @@ func toPtr[T any](t T) *T {
 
 func TestUserCreate(t *testing.T) {
 	repo := &mocks.UserRepository{}
+	expectedID := int64(1)
 	repo.On(
 		"Create",
 		mock.AnythingOfType("*context.emptyCtx"),
 		mock.AnythingOfType("domain.User")).
-		Return(int64(1), nil)
+		Return(expectedID, nil)
 
 	usecase := NewUsecase(repo, hash.NewSHA256Hasher([]byte("salt")))
-	_, err := usecase.Create(context.Background(), domain.CreateUserDTO{
+	resultID, err := usecase.Create(context.Background(), domain.CreateUserDTO{
 		Login:    toPtr("login"),
 		Name:     toPtr("name"),
 		Password: toPtr("password"),
 	})
 
+	assert.Equal(t, expectedID, resultID)
 	assert.Nil(t, err)
 }
 
 func TestUserCreateError(t *testing.T) {
 	repo := &mocks.UserRepository{}
+	expectedID := int64(0)
 	repo.On(
 		"Create",
 		mock.AnythingOfType("*context.emptyCtx"),
 		mock.AnythingOfType("domain.User")).
-		Return(int64(0), domain.ErrUserAlreadyExists)
+		Return(expectedID, domain.ErrUserAlreadyExists)
 
 	usecase := NewUsecase(repo, hash.NewSHA256Hasher([]byte("salt")))
-	_, err := usecase.Create(context.Background(), domain.CreateUserDTO{
+	resultID, err := usecase.Create(context.Background(), domain.CreateUserDTO{
 		Login:    toPtr("login"),
 		Name:     toPtr("name"),
 		Password: toPtr("password"),
 	})
 
+	assert.Equal(t, expectedID, resultID)
 	assert.ErrorIs(t, err, domain.ErrUserAlreadyExists)
+}
+
+func TestGetUserID(t *testing.T) {
+	repo := &mocks.UserRepository{}
+	expectedID := int64(1)
+	repo.On(
+		"GetID",
+		mock.AnythingOfType("*context.emptyCtx"),
+		mock.AnythingOfType("string"),
+		mock.AnythingOfType("string")).
+		Return(expectedID, nil)
+
+	usecase := NewUsecase(repo, hash.NewSHA256Hasher([]byte("salt")))
+	resultID, err := usecase.GetID(context.Background(), domain.GetUserIDDTO{
+		Login:    toPtr("login"),
+		Password: toPtr("password"),
+	})
+
+	assert.Equal(t, expectedID, resultID)
+	assert.Nil(t, err)
 }
